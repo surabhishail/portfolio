6781fc93155b46080473677ef6ae6ff829b147c5
Surabhi Shail <Surabhishail@users.noreply.github.com>
2020-07-15 15:28:37 -0400

    Updates

---
+++ precache-manifest.a34e63cd3118293004b25be6c22ccc99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c78a0bd7c04ce6d5f15f",
+    "url": "/portfolio/static/css/main.e45a7e72.chunk.css"
+  },
+  {
+    "revision": "c78a0bd7c04ce6d5f15f",
+    "url": "/portfolio/static/js/main.c78a0bd7.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "4773bb160512533240d2cdf9b8bc956e",
+    "url": "/portfolio/index.html"
+  }
+];
